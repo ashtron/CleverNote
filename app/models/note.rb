b316d820d150ac0de2950b658c2e04d20b0d7b8f
ashtron <ashtonplaysted@gmail.com>
2015-10-13 10:03:57 -0700

    Implemented user authentication

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: notes
+#
+#  id          :integer          not null, primary key
+#  title       :string           not null
+#  body        :text             not null
+#  author_id   :integer          not null
+#  notebook_id :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  archived    :boolean          default(FALSE)
+#
+
 class Note < ActiveRecord::Base
+  belongs_to :user
 end
