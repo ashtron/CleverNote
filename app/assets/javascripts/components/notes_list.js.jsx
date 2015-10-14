f9bbe6f31e2aa520879183c3d8b6a49f6d2c3fc1
ashtron <ashtonplaysted@gmail.com>
2015-10-13 17:30:50 -0700

    Solved CSS floating issue

--- app/assets/javascripts/components/notes_list.js.jsx
+++ app/assets/javascripts/components/notes_list.js.jsx
@@ -1,16 +1,28 @@
 /* globals React, NoteStore, NoteListItem */
 
 var NotesList = React.createClass({
+  _onChange: function() {
+    this.setState({ notes: NoteStore.all() });
+  },
+
+  getInitialState: function() {
+    return { notes: NoteStore.all() };
+  },
+
+  componentDidMount: function() {
+    this.setState({ notes: NoteStore.all() });
+    NoteStore.addChangeListener(this._onChange);
+  },
+
   render: function() {
     return (
       <div className="notes-list">
         <div className="notes-list-header">Notes</div>
-        {
-          NoteStore.all().map(function(note) {
-            debugger;
-            return (<NoteListItem>{note.title}</NoteListItem>);
-          })
-        }
+          {
+            NoteStore.all().map(function(note) {
+              return (<NoteListItem>{note.title}</NoteListItem>);
+            })
+          }
       </div>
     );
   }
