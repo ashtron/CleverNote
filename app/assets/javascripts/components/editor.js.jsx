98c4e560404366012284d045f3cdee9f1a32124b
ashtron <ashtonplaysted@gmail.com>
2015-10-15 16:33:32 -0700

    Cleaned up editor html

--- app/assets/javascripts/components/editor.js.jsx
+++ app/assets/javascripts/components/editor.js.jsx
@@ -21,41 +21,56 @@ var Editor = React.createClass({
       			</select>
       		</span>
 
-      		<span className="ql-format-group"><span title="Bold" className="ql-format-button ql-bold"></span><span className="ql-format-separator"></span><span title="Italic" className="ql-format-button ql-italic"></span><span className="ql-format-separator"></span><span title="Underline" className="ql-format-button ql-underline"></span><span className="ql-format-separator"></span><span title="Strikethrough" className="ql-format-button ql-strike"></span></span>
-
+      		<span className="ql-format-group">
+            <span title="Bold" className="ql-format-button ql-bold"></span>
+            <span className="ql-format-separator"></span>
+            <span title="Italic" className="ql-format-button ql-italic"></span>
+            <span className="ql-format-separator"></span>
+            <span title="Underline" className="ql-format-button ql-underline"></span>
+            <span className="ql-format-separator"></span>
+            <span title="Strikethrough" className="ql-format-button ql-strike"></span>
+          </span>
 
       		<span className="ql-format-group">
       			<span title="List" className="ql-format-button ql-list"></span>
             <span className="ql-format-separator"></span>
             <span title="Bullet" className="ql-format-button ql-bullet"></span>
-
-          <span className="ql-format-group">
-            <span title="Text Alignment" className="ql-align ql-picker">
-              <span className="ql-picker-options">
-                <span data-value="left" className="ql-picker-item ql-selected"></span>
-                <span data-value="center" className="ql-picker-item"></span>
-                <span data-value="right" className="ql-picker-item"></span>
-                <span data-value="justify" className="ql-picker-item"></span>
-              </span>
-            </span>
           </span>
 
+          <span className="ql-format-group">
       			<select title="Text Alignment" className="ql-align" >
       				<option value="left" label="Left" selected=""></option>
       				<option value="center" label="Center"></option>
       				<option value="right" label="Right"></option>
       				<option value="justify" label="Justify"></option>
       			</select>
-      		</span>
+          </span>
 
-      		<span className="ql-format-group"><span title="Link" className="ql-format-button ql-link"></span></span>
+      		<span className="ql-format-group">
+            <span title="Link" className="ql-format-button ql-link">
+            </span>
+          </span>
+
+          <span className="toolbar-title">Bill's Cool Note</span>
       	</div>
 
       	<div id="editor" className="editor ql-container ql-snow">
-      		<div className="ql-multi-cursor"><span className="cursor left hidden" ><span className="cursor-flag"> <span className="cursor-triangle top" ></span> <span className="cursor-name" >Gandalf</span> <span className="cursor-triangle bottom" ></span> </span> <span className="cursor-caret" ></span></span></div>
+      		<div className="ql-multi-cursor">
+            <span className="cursor left hidden">
+              <span className="cursor-flag">
+                <span className="cursor-triangle top"></span>
+                <span className="cursor-name">Gandalf</span>
+                <span className="cursor-triangle bottom"></span>
+              </span>
+
+              <span className="cursor-caret"></span>
+            </span>
+          </div>
+
       		<div className="ql-editor authorship" id="ql-editor-2" contenteditable="true">
       			<div>Start typing.</div>
       		</div>
+
       		<div className="ql-paste-manager" contenteditable="true"></div>
       	</div>
       </div>
