d5ca2dab9c9e0241457acd8e905660fe1f13d460
ashtron <ashtonplaysted@gmail.com>
2015-10-16 15:25:23 -0700

    Fixed spacing issues in CSS

--- app/assets/javascripts/components/navbar.js.jsx
+++ app/assets/javascripts/components/navbar.js.jsx
@@ -14,7 +14,7 @@ var Navbar = React.createClass({
 
   render: function() {
     return (
-      <nav className="navbar navbar-default">
+      <nav id="nav-container" className="navbar navbar-default">
         <div className="container-fluid">
           <div className="navbar-header">
             <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
