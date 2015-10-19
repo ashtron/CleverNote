ca843649cc8bc30bd639f742d9d7cda6cecacf80
ashtron <ashtonplaysted@gmail.com>
2015-10-19 10:56:03 -0700

    Clicking a notebook renders list of notes

--- app/assets/javascripts/components/notebooks_page.js.jsx
+++ app/assets/javascripts/components/notebooks_page.js.jsx
@@ -1,11 +1,28 @@
-/* globals React, NotebooksList, Editor */
+/* globals React, NotebooksList, NotesList, NotebookEditor, NotebookStore */
+/* globals NoteEditor */
 
 var NotebooksPage = React.createClass({
+  getInitialState: function() {
+    return { list: <NotebooksList/>, editor: <NotebookEditor/> };
+  },
+
+  _onChange: function() {
+    if ($.isEmptyObject(NotebookStore.selectedNotebook())) {
+      this.setState({ list: <NotebooksList/>, editor: <NotebookEditor/> });
+    } else {
+      this.setState({ list: <NotesList/>, editor: <NoteEditor/> });
+    }
+  },
+
+  componentDidMount: function() {
+    NotebookStore.addChangeListener(this._onChange);
+  },
+
   render: function() {
     return (
       <div>
-        <NotebooksList/>
-        <NotebookEditor/>
+        {this.state.list}
+        {this.state.editor}
       </div>
     );
   }
